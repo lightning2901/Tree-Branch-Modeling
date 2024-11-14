77a2294348f58ac74189c134269ff482f3b6edc5
zeogarca <124711181+zeogarca@users.noreply.github.com>
2024-11-14 14:11:25 -0600

    Create matrix_rot.py

---
+++ matrix_rot.py
@@ -0,0 +1,20 @@
+def rotate_x(theta):
+    return np.array([
+        [1, 0, 0],
+        [0, np.cos(theta), -np.sin(theta)],
+        [0, np.sin(theta), np.cos(theta)]
+    ])
+
+def rotate_y(theta):
+    return np.array([
+        [np.cos(theta), 0, np.sin(theta)],
+        [0, 1, 0],
+        [-np.sin(theta), 0, np.cos(theta)]
+    ])
+
+def rotate_z(theta):
+    return np.array([
+        [np.cos(theta), -np.sin(theta), 0],
+        [np.sin(theta), np.cos(theta), 0],
+        [0, 0, 1]
+    ])
